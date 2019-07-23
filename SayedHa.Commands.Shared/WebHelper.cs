872231042d16920fff87ccad2cef682d9a936aba
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-07-23 00:20:07 -0400

    work in progress

--- SayedHa.Commands.Shared/WebHelper.cs
+++ SayedHa.Commands.Shared/WebHelper.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using SayedHa.Commands.Shared.Extensions;
 
 namespace SayedHa.Commands.Shared {
     public class WebHelper {
-        public void DownloadFile(string url, string destFilepath, bool allowOverwrite = false) {
+        public async Task DownloadFile(string url, string destFilepath, bool allowOverwrite = false) {
             Debug.Assert(!string.IsNullOrEmpty(url));
             Debug.Assert(!string.IsNullOrEmpty(destFilepath));
 
@@ -13,8 +15,14 @@ namespace SayedHa.Commands.Shared {
                 throw new FileExistsException($"File already exists at destination, and overwrite is set to false, path='{destFilepath}'");
             }
 
+            using var client = new WebClient();
+            var data = client.DownloadData(url);
+            using var content = new MemoryStream(data);
+            using var outstream = new FileStream(destFilepath, FileMode.Create);
+            await content.CopyToAsync(outstream);
 
-            throw new NotImplementedException();
+            content.Close();
+            outstream.Close();
         }
     }
 }
---
+++ SayedHa.Commands.Test/TestWebHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using SayedHa.Commands.Shared;
+using Xunit;
+
+namespace SayedHa.Commands.Test {
+    public class TestWebHelper {
+        [Fact]
+        public async System.Threading.Tasks.Task TestDownloadFileSuccess01Async() {
+            string url = @"https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore";
+            string destFile = Path.GetTempFileName();
+            
+            if (File.Exists(destFile)) File.Delete(destFile);
+
+            Assert.True(!File.Exists(destFile));
+            await new WebHelper().DownloadFile(url, destFile, true);
+            Assert.True(File.Exists(destFile));
+            
+
+        }
+    }
+}
