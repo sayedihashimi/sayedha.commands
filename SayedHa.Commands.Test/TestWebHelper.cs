41032a6acd7689460eba662f689c675984a43e79
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-07-24 23:00:34 -0400

    adding test

--- SayedHa.Commands.Test/TestWebHelper.cs
+++ SayedHa.Commands.Test/TestWebHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using SayedHa.Commands.Shared;
 using Xunit;
 
 namespace SayedHa.Commands.Test {
     public class TestWebHelper {
         [Fact]
-        public async System.Threading.Tasks.Task TestDownloadFileSuccess01Async() {
+        public async Task TestDownloadFileSuccess01Async() {
             string url = @"https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore";
             string destFile = Path.GetTempFileName();
             
@@ -15,8 +17,21 @@ namespace SayedHa.Commands.Test {
             Assert.True(!File.Exists(destFile));
             await new WebHelper().DownloadFile(url, destFile, true);
             Assert.True(File.Exists(destFile));
-            
+        }
 
+        [Fact]
+        public async Task TestDownloadFileNotExists() {
+            string url = @"https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignoreddddddddddd";
+            string destFile = Path.GetTempFileName();
+
+            if (File.Exists(destFile)) File.Delete(destFile);
+
+            try {
+                await new WebHelper().DownloadFile(url, destFile, true);
+            }
+            catch(WebException ex) {
+                Assert.True(WebExceptionStatus.ProtocolError == ex.Status);
+            }
         }
     }
 }
