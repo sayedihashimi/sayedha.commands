1b3543a2648aceea0ad25584d808864f076bd2ed
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-04-17 15:21:52 -0400

    adding support for LoremIpsum

--- SayedHa.Commands.Shared/MissingParameterException.cs
+++ SayedHa.Commands.Shared/MissingParameterException.cs
@@ -1,7 +1,35 @@
 ﻿using System;
 namespace SayedHa.Commands.Shared {
-    public class MissingParameterException {
+
+    [Serializable]
+    public class MissingParameterException : Exception {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MissingParameterException"/> class
+        /// </summary>
         public MissingParameterException() {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MissingParameterException"/> class
+        /// </summary>
+        /// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+        public MissingParameterException(string message) : base(message) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MissingParameterException"/> class
+        /// </summary>
+        /// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
+        /// <param name="inner">The exception that is the cause of the current exception. </param>
+        public MissingParameterException(string message, System.Exception inner) : base(message, inner) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MissingParameterException"/> class
+        /// </summary>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        protected MissingParameterException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) {
         }
     }
 }
